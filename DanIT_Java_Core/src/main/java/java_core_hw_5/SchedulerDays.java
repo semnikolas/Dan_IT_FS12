8dedf7c85f0c0f5f2d5b02ac4b8af55c9f28b088
semnikolas <nikolaychuk.simon@gmail.com>
2025-11-06 22:26:55 +0200

    hw_5_in_progress

---
+++ DanIT_Java_Core/src/main/java/java_core_hw_5/SchedulerDays.java
@@ -0,0 +1,23 @@
+package java_core_hw_5;
+
+public enum SchedulerDays {
+
+
+    MON("Monday"),
+    TUE("Tuesday"),
+    WED("Wednesday"),
+    THU("Thursday"),
+    FRI("Friday"),
+    SAT("Saturday"),
+    SUN("Sunday");
+
+    private final String title;
+
+    SchedulerDays(String title) {
+        this.title = title;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+}
